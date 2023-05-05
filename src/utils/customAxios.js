853969bbff5592e14ebd8cf559b897538fc4d59e
burhanthecoder <burhandilsad63@gmail.com>
2023-05-05 11:54:19 +0530

    deploying

--- src/utils/customAxios.js
+++ src/utils/customAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const customAxios = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
     'Authorization': `Bearer ${localStorage.getItem("token") || ""}`,
